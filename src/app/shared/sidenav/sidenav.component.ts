5085cadf4f93b0f1afff29b46ba34d054aaff1f3
harrissondutra <harrissondutra@gmail.com>
2025-06-11 23:21:31 -0300

    Menu lateral

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -2,15 +2,17 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'; // <-- Adicione esta linha
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  standalone: true, // <-- Adicione esta linha
+  standalone: true,
   imports: [
     CommonModule,
     MatListModule,
     MatIconModule,
+    MatExpansionModule, // <-- E esta linha
     RouterModule
   ],
   templateUrl: './sidenav.component.html',
