573dd402611f9319a60322a551bf662ef31f2c1b
harrissondutra <harrissondutra@gmail.com>
2025-06-15 12:20:46 -0300

    fix: uncomment API URL for development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080', // URL da API em desenvolvimento
+//  apiUrl: 'http://localhost:8080', // URL da API em desenvolvimento
+ apiUrl: 'https://api-sistudo-fitness.fly.dev',
 };
