ae3335743b22a6c97c4d62e2e58be252dc41ded6
harrissondutra <harrissondutra@gmail.com>
2025-06-16 00:38:51 -0300

    feat: enhance sidenav component with collapsible functionality and icon support for menu items

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,12 +1,13 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 interface MenuItem {
   title: string;
+  icon?: string;
   links: {
     label: string;
     route: string;
@@ -15,7 +16,9 @@ interface MenuItem {
 
 @Component({
     selector: 'app-sidenav',
+    standalone: true,
     imports: [
+    CommonModule,
     MatListModule,
     MatIconModule,
     MatExpansionModule,
@@ -26,14 +29,17 @@ interface MenuItem {
 })
 export class SidenavComponent {
   @Output() linkClicked = new EventEmitter<void>();
+  @Output() expandClicked = new EventEmitter<void>();
+  @Input() collapsed = false;
 
   // Novo item para o link Home direto
-  homeLink = { label: 'Home', route: '/' };
+  homeLink = { label: 'Home', route: '/', icon: 'home' };
 
-  // Itens de menu que terão painéis de expansão (anteriormente 'menuItems')
+  // Itens de menu que terão painéis de expansão
   expandableMenuItems: MenuItem[] = [
     {
       title: 'Clientes',
+      icon: 'person',
       links: [
         { label: 'Listar Clientes', route: '/users-list' },
         { label: 'Criar Novo Cliente', route: '/register' },
@@ -41,25 +47,33 @@ export class SidenavComponent {
     },
     {
       title: 'Treinos',
+      icon: 'fitness_center',
       links: [
         { label: 'Exibir Treinos', route: '/trainning' },
         { label: 'Inativos', route: '/inactive-trainning' },
         { label: 'Criar Novo Treino', route: '/trainning-create' },
-
       ]
     },
     {
       title: 'Exercícios',
+      icon: 'sports_gymnastics',
       links: [
         { label: 'Listar Exercícios', route: '/exercises' },
-        { label: 'Inserir Categoria', route: 'category-exercise' },
+        { label: 'Inserir Categoria', route: '/category-exercise' },
         { label: 'Criar Novo Exercício', route: '/create-exercise' },
-
       ]
     },
   ];
 
+  toggleSidenav() {
+    this.collapsed = !this.collapsed;
+  }
+
   onLinkClick() {
     this.linkClicked.emit();
+  }
+
+  expandSidenav() {
+    this.expandClicked.emit();
   }
 }
