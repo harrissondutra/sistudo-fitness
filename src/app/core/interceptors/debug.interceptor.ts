2d4b28f907793315e56c28c236963b2da87890f1
harrissondutra <harrissondutra@gmail.com>
2025-08-12 18:24:23 -0300

    fix: remove unnecessary blank line in debug interceptor

--- src/app/core/interceptors/debug.interceptor.ts
+++ src/app/core/interceptors/debug.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const debugInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('🔥 [DEBUG INTERCEPTOR] FUNCIONANDO! URL:', req.url);
-  
+
   // Adicionar header de teste
   const modifiedReq = req.clone({
     setHeaders: {
